c74ad54b1903a0ca94d50f4fc9baf4b8767b8036
Justin Zhang <73123028+justin-himself@users.noreply.github.com>
2022-09-27 21:16:49 +0800

    Update scraper.py

--- .github/workflows/scraper.py
+++ .github/workflows/scraper.py
@@ -11,7 +11,7 @@ from time import sleep
 
 class GCEScraper:
 
-    def __init__(self, save_path, worker_num=10, worker_delay=0.1, max_size=0, tui=False, attachment_suffix=[".pdf", ".zip"], max_retries=10) -> None:
+    def __init__(self, save_path, worker_num=10, worker_delay=0.1, max_size=0, tui=False, attachment_suffix=[".pdf", ".zip"], max_retries=10, download_limit=0) -> None:
         self.task_queue = queue.Queue(max_size)
         self.error_queue = queue.Queue(max_size)
         self.state_dict = {i: "" for i in range(0, worker_num)}
@@ -21,9 +21,11 @@ class GCEScraper:
         self.worker_num = worker_num
         self.worker_delay = worker_delay
         self.attachment_suffix = attachment_suffix
-        self.max_retries = 10
+        self.max_retries = max_retries
+        self.download_limit = download_limit
 
         self.terminated = False
+        self.download_count = 0
 
     def add_url(self, url_list):
         [self.task_queue.put([url, self.max_retries]) for url in url_list]
@@ -35,38 +37,56 @@ class GCEScraper:
                     self.state_dict[i] = text
                 else:
                     print("worker " + str(i) + " : " + text)
+
             return _update_state
 
         # create workers
         self.workers = [threading.Thread(target=self._worker, args=(
-            "worker-" + str(i), self.worker_delay, update_state_func(i))) for i in range(0, self.worker_num)]
+            self.worker_delay, update_state_func(i))) for i in range(0, self.worker_num)]
 
         # start workers
         [worker.start() for worker in self.workers]
 
-        if self.tui:
-            self.live_display_worker = threading.Thread(
-                target=self._live_display)
-            self.live_display_worker.start()
+        # start other superviours
+        self.supervisor = threading.Thread(
+            target=self._supervisor)
+        self.supervisor.start()
 
         # end
         self.task_queue.join()
         self.terminated = True
 
-        print(list(self.error_queue))
-        return list(self.error_queue)
+        error_list = []
+        while not self.error_queue.empty():
+            error_list.append(self.error_queue.get())
+        return error_list
 
-    def _live_display(self):
-        sys.stdout.write("\n" * self.worker_num)
-        while not self.terminated:
+    def _supervisor(self):
+        if self.tui:
+            sys.stdout.write("\n" * (self.worker_num + 1))
+        while threading.active_count() > 2:
+            # supervisor, main + workers
             sleep(0.1)
-            # if tui, rewrite multiple lines
-            for i in range(0, self.worker_num):
-                sys.stdout.write("\x1b[1A\x1b[2K")
+            # count
+            if self.download_limit != 0:
+                if self.download_count > self.download_limit:
+                    self.terminated = True
 
-            for key in self.state_dict:
-                sys.stdout.write("worker" + str(key) + ":\t" +
-                                 self.state_dict[key] + "\n")
+            # printing
+            if self.tui:
+
+                for i in range(0, self.worker_num + 1):
+                    sys.stdout.write("\x1b[1A\x1b[2K")
+
+                sys.stdout.write("count: \t" +
+                                 str(threading.active_count()-2) + "\n")
+
+                for key in self.state_dict:
+                    sys.stdout.write("worker" + str(key) + ":\t" +
+                                     self.state_dict[key] + "\n")
+
+        while self.task_queue.unfinished_tasks > 0:
+            self.task_queue.task_done()
 
     def _html_url_handler(self, abs_url) -> list:
         if not abs_url.endswith("/"):
@@ -101,7 +121,10 @@ class GCEScraper:
         with open(path, "wb") as file_on_disk:
             file_on_disk.write(file)
 
-    def _worker(self, worker_name, delay, update_state):
+        # update download count
+        self.download_count += 1
+
+    def _worker(self,  delay, update_state):
         while not self.terminated:
             sleep(delay)
             if self.task_queue.empty():
@@ -135,7 +158,7 @@ class GCEScraper:
 
             self.task_queue.task_done()
 
-        print(worker_name + " terminated")
+        update_state("TERMINATED")
 
 
 def main():
@@ -144,26 +167,28 @@ def main():
     parser.add_argument("save_location", metavar="path",
                         type=str, help="parent dir to save the scraped files.", default="/")
     parser.add_argument(
-        "-t", "--thread", type=int, help="number of parallel workers", metavar="int", default=50)
+        "--thread", type=int, help="number of parallel workers", metavar="int", default=50)
     parser.add_argument(
-        "-d", "--delay", type=float, help="delay after each request", metavar="float",  default=0)
+        "--delay", type=float, help="delay after each request", metavar="float",  default=0)
     parser.add_argument("-r", "--retries", type=int,
                         help="maximum retries after giving up a url", metavar="int", default=10)
     parser.add_argument(
         "--tui", help="friendly user interface", action="store_true")
-    parser.add_argument("-s", "--suffix", dest="attachment_suffix", type=str,
+    parser.add_argument(
+        "--limit", help="limit how many files can download", type=int, metavar="int",  default=0)
+    parser.add_argument("--suffix", dest="attachment_suffix", type=str,
                         help="url with given suffix will be downloaded", metavar="suffix", nargs="+", default=[".pdf"])
 
     args = parser.parse_args()
 
     gce_scraper = GCEScraper(
-        args.save_location, worker_num=args.thread, worker_delay=args.delay, tui=args.tui, attachment_suffix=args.attachment_suffix)
+        args.save_location, worker_num=args.thread, worker_delay=args.delay, tui=args.tui, attachment_suffix=args.attachment_suffix, download_limit=args.limit)
     gce_scraper.add_url(
         ["https://papers.gceguide.com/"])
 
     error_list = gce_scraper.run()
 
-    with open(args.save_location + "scraper_errors.json", "w") as debugfile:
+    with open(os.path.join(args.save_location + "/scraper_errors.json"), "w") as debugfile:
         debugfile.write("\n".join(error_list))
 
 
