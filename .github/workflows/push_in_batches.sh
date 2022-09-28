6319bf3e54ed5801ba128e9b459a6b93ec1ce952
Justin Zhang <73123028+justin-himself@users.noreply.github.com>
2022-09-28 18:13:34 +0800

    Update push_in_batches.sh

--- .github/workflows/push_in_batches.sh
+++ .github/workflows/push_in_batches.sh
@@ -5,13 +5,15 @@ SIZE_LIMIT=2040109466
 
 function git_push(){
     git add $*
-    git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-    git config --local user.name "github-actions[bot]"
     git commit -m "commit in group size of $SIZE_LIMIT" -a
-
-    remote_repo="https://${GITHUB_ACTOR}:${repo-token}@github.com/${GITHUB_REPOSITORY}.git"
     git push "${remote_repo}" HEAD:${GITHUB_REF_NAME} --verbose
 }
+
+
+git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
+remote_repo="https://${GITHUB_ACTOR}:${repo-token}@github.com/${GITHUB_REPOSITORY}.git"
+git config --local --add safe.directory .
 
 
 file_list=$(git status -u | grep -P "^\t" | xargs wc -c | sed '$d' | awk '{$1=$1};1')
