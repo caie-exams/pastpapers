aecd258993bbaca87f43e8aab0786adf6387fac8
Justin Zhang <73123028+justin-himself@users.noreply.github.com>
2022-09-28 14:04:52 +0800

    Create push_in_batches.sh

---
+++ .github/workflows/push_in_batches.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -e
+
+SIZE_LIMIT=2040109466
+
+function git_push(){
+    git add $*
+    git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+    git config --local user.name "github-actions[bot]"
+    git commit -m "commit in group size of $SIZE_LIMIT" -a
+
+    remote_repo="https://${GITHUB_ACTOR}:${{ secrets.GH_TOKEN }}@${GITHUB_REPOSITORY}.git"
+    git push "${remote_repo}" HEAD:${GITHUB_REF_NAME} --verbose
+}
+
+
+file_list=$(git status -u | grep -P "^\t" | xargs wc -c | sed '$d' | awk '{$1=$1};1')
+submit_list=""
+submit_size=0
+
+while [ "$file_list" != "" ];
+do
+    # take each line and add to another list
+    
+    newline=${file_list%%$'\n'*}
+    submit_size=$(echo "$submit_size  + ${newline% *}" | bc -l)
+    
+    if [ $submit_size -ge $SIZE_LIMIT ];
+        then
+            git_push $submit_list
+
+            submit_size=0
+            submit_list=""
+        else
+            submit_list+="${newline#* } " 
+            file_list=$(echo "$file_list" | sed '1d')
+    fi
+done
+
+
+if [ "$submit_list" != "" ];
+    then
+        git_push $submit_list
+fi
+
